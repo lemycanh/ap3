3ecbc2a834823ffc323eed64c631f6c445b10065
Canh Le My <lemycanh@gmail.com>
2020-10-23 15:37:15 +0700

    * Helloworld

---
+++ app/src/main/java/com/uni/geoquiz/MainActivity.java
@@ -0,0 +1,28 @@
+package com.uni.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.activity_main,null);
+        setContentView(view);
+    }
+
+    public void btTrue_OnClick(View view) {
+        Toast.makeText(this, "Bạn đã trả lời ĐÚNG", Toast.LENGTH_SHORT).show();
+    }
+
+    public void btFalse_OnClick(View view) {
+        Toast.makeText(this, "Bạn đã trả lời SAI", Toast.LENGTH_SHORT).show();
+    }
+}
